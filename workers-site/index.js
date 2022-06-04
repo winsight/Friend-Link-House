a3110a221a5460ae1f6eb9f48fcbb8860fb2b12d
idealclover <idealclover@live.com>
2022-06-04 23:13:29 +0800

    :tada: add Access-Control-Allow-Origin

--- workers-site/index.js
+++ workers-site/index.js
@@ -51,6 +51,7 @@ async function handleEvent(event) {
     response.headers.set("X-Frame-Options", "DENY");
     response.headers.set("Referrer-Policy", "unsafe-url");
     response.headers.set("Feature-Policy", "none");
+    response.headers.set('Access-Control-Allow-Origin', "*");
 
     return response;
 
